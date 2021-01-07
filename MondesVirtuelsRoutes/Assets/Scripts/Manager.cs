e52355386f34b96be9b478b53b27c8648d2bb7c3
valentinLampriere <valentin.lampriere1@gmail.com>
2021-01-07 19:37:44 +0100

    Road maps the terrain

--- MondesVirtuelsRoutes/Assets/Scripts/Manager.cs
+++ MondesVirtuelsRoutes/Assets/Scripts/Manager.cs
@@ -8,19 +8,29 @@ public class Manager : MonoBehaviour {
     private string file;
     [SerializeField]
     private GameObject emptyRoad;
+    [SerializeField]
+    private GameObject terrain;
 
     private Root root;
     private List<Road> roads;
+
+    [SerializeField]
+    private bool fixTerrainCoord;
 
 
     void Start() {
         roads = new List<Road>();
         root = JsonParser.Parse("Assets/Data/" + file);
 
+        if (fixTerrainCoord)
+            FixTerrainCoord();
         SetUpRoads();
-
         DrawRoads();
+    }
 
+    private void FixTerrainCoord() {
+        Mesh mesh = terrain.GetComponentInChildren<MeshFilter>().mesh;
+        terrain.transform.GetChild(0).RotateAround(mesh.bounds.center, Vector3.right, -90.0f);
     }
 
     /// <summary> Link roads coordinates with LineRenderer </summary>
@@ -40,12 +50,15 @@ public class Manager : MonoBehaviour {
 
     /// <summary> Create roads objects from JSON </summary>
     void SetUpRoads() {
-
         float xOffset = (float)root.features[0].geometry.coordinates[0][0][0];
         float zOffset = (float)root.features[0].geometry.coordinates[0][0][1];
 
         // For each enties in the json
         foreach (Feature f in root.features) {
+
+            int previousIndexTriangle = -1;
+            int currentIndexTriangle;
+
             int importance;
             string nom, id;
             float largeur;
@@ -65,13 +78,66 @@ public class Manager : MonoBehaviour {
             // Loop through each point of the road
             foreach (List<double> l in f.geometry.coordinates[0]) {
                 float x = (float)l[0] - xOffset;
-                float y = 0;
                 float z = (float)l[1] - zOffset;
+                float y = GetAltitudeFromMap(new Vector3(x, 0, z), out currentIndexTriangle);
+
+                if (previousIndexTriangle > 0 && currentIndexTriangle != previousIndexTriangle) {
+                    // Il faut trouver l'arrete commune, et rajouter un point au niveau de l'arrete
+
+                    // récuperer les triangles
+                    // check si 2 vertices sont en commun
+
+                    // triangles[indice] -> tableau de vertices
+
+                    Mesh mesh = terrain.GetComponentInChildren<MeshFilter>().mesh;
+
+                    List<Vector3> previousVertices = new List<Vector3> {
+                        mesh.vertices[mesh.triangles[previousIndexTriangle * 3 + 0]],
+                        mesh.vertices[mesh.triangles[previousIndexTriangle * 3 + 1]],
+                        mesh.vertices[mesh.triangles[previousIndexTriangle * 3 + 2]]
+                    };
+                    List<Vector3> currentVertices = new List<Vector3> {
+                        mesh.vertices[mesh.triangles[currentIndexTriangle * 3 + 0]],
+                        mesh.vertices[mesh.triangles[currentIndexTriangle * 3 + 1]],
+                        mesh.vertices[mesh.triangles[currentIndexTriangle * 3 + 2]]
+                    };
+                    List<Vector3> sharedVertices = new List<Vector3>();
+                    for (int i = 0; i < previousVertices.Count; i ++) {
+                        for (int j = 0; j < currentVertices.Count; j++) {
+                            if (previousVertices[i] == currentVertices[j]) {
+                                if (! sharedVertices.Contains(previousVertices[i]))
+                                    sharedVertices.Add(previousVertices[i]);
+                            }
+                        }
+                    }
+                    if (sharedVertices.Count == 2) {
+                        // calculer un point sur l'arrete
+
+                        Vector2 roadDirection =
+
+                    }
+                }
+
+                previousIndexTriangle = currentIndexTriangle;
 
                 positions.Add(new Vector3(x, y, z));
             }
 
             roads.Add(new Road(positions, largeur, nom, importance, id));
         }
+    }
+
+    /// <summary>Get the Y coordinate which will fit with the terrain depending on X & Z or 0.0</summary>
+    private float GetAltitudeFromMap(Vector3 pos, out int indexTriangle) {
+        RaycastHit hit;
+        indexTriangle = 0;
+        if (Physics.Raycast(pos, Vector3.down, out hit)) {
+            indexTriangle = hit.triangleIndex;
+            return hit.point.y;
+        } else if (Physics.Raycast(pos, Vector3.up, out hit)) {
+            indexTriangle = hit.triangleIndex;
+            return hit.point.y;
+        }
+        return 0.0f;
     }
 }
